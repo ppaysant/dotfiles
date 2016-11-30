7433494ad15f8e56227bf78cb2457ec9b6fe4f58
Patrick Paysant <patrick.paysant@linagora.com>
2016-11-30 09:29:59 +0100

    Add chromium to start script and root profile to term

--- bin/start.sh
+++ bin/start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-MATTERMOST_CLIENT="/home/LOGIN/bin/mattermost-desktop-linux-x64/Mattermost &"
-PIDGIN="pidgin &"
-HEXCHAT="/usr/bin/hexchat &"
-TERM="gnome-terminal --geometry 164x39 &"
+MATTERMOST_CLIENT="/home/patrick/bin/mattermost-desktop-linux-x64/Mattermost"
+PIDGIN="pidgin"
+HEXCHAT="/usr/bin/hexchat"
+TERM="gnome-terminal --geometry 164x39"
+CHROMIUM="chromium"
 
 
 # ===== "Mattermost"
-${MATTERMOST_CLIENT}
+${MATTERMOST_CLIENT} & 
 wid=$(xdotool search --sync --all --onlyvisible --name "Mattermost")
 sleep 1
 
@@ -17,10 +18,10 @@ sleep 1
 xdotool mousemove --window $wid 500 380 click 1
 
 # ===== "Pidgin"
-${PIDGIN}
+${PIDGIN} &
 
 # ===== "hexchat"
-${HEXCHAT}
+${HEXCHAT} &
 sleep 1
 wid=$(xdotool search --sync --all --onlyvisible --name "hexchat")
 
@@ -30,10 +31,20 @@ xdotool windowsize $wid 1400 920
 xdotool windowmove $wid 132 80
 
 # ===== "Terminal"
-${TERM}
+${TERM} &
 sleep 1
 wid=$(xdotool search --sync --all --onlyvisible --class "gnome-terminal" | grep -v $WINDOWID)
 
 xdotool windowmove $wid 100 175
 xdotool windowactivate $wid key ctrl+shift+t
+xdotool key alt+t Right Down KP_Enter
 xdotool windowsize $wid 1450 730
+
+# ===== "Chromium"
+xdotool set_desktop 1
+${CHROMIUM} &
+sleep 1
+wid=$(xdotool search --sync --all --onlyvisible --class "chromium" | tail -1)
+
+xdotool windowactivate $wid key alt+F10
+
